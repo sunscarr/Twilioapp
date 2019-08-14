a339786821ba960f5e9e08bb87dd89f2e48f5bc2
Sanskar Gyawali <sgyawali@arcusbio.com>
2019-08-14 15:24:12 -0700

    trying

--- twilio.js
+++ twilio.js
@@ -292,6 +292,4 @@ http.createServer(app).listen(1337, () => {
   console.log('Express server listening on port 1337');
 });
 */
-app.listen(process.env.PORT || 1337, () => {
-  console.log('Express server listening on port 1337');
-});
+app.listen(process.env.PORT || 1337, () => console.log("listening"));
