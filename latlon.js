20bb6743238b3dca250e3857a46ae0d2f9703b73
Sanskar Gyawali <sgyawali@arcusbio.com>
2019-08-19 14:13:10 -0700

    added commands

--- latlon.js
+++ latlon.js
@@ -5,12 +5,12 @@ const baseurl="https://api.opencagedata.com/geocode/v1/json?q="
 
 function uslonglat(city, state, country, callback){
   var key = baseurl+city+"&key="+latAPI;
-//  console.log(key);
+  console.log(key);
   request(key,{json:true}, (err,res,body) =>{
     for(i in body.results){
       var bod=body.results[i];
       if(bod.components._type == "city"){
-        if(bod.components.city.toUpperCase()== city.toUpperCase() && bod.components.country.toUpperCase()== country.toUpperCase() && bod.components.state_code.toUpperCase()== state.toUpperCase()){
+        if(bod.components.city.toUpperCase()== city.toUpperCase() && bod.components.country.toUpperCase()== country.toUpperCase() && bod.components.state_code.toUpperCase()== state.toUpperCase() ){
           callback(bod.geometry.lat+" "+bod.geometry.lng);
           break;
         }
@@ -27,16 +27,36 @@ uslonglat("San Francisco","CA","USA", function(bd){
 
 function getlonglat(city, country, callback){
   var key = baseurl+city+"&key="+latAPI;
-//  console.log(key);
+  console.log(key);
+  if (country.toUpperCase()=="CHINA"){
+    country = "PRC";
+  }
   request(key,{json:true}, (err,res,body) =>{
+    if(body.total_results ==0){
+      callback("Couldn't find, try again")
+    }
     for(i in body.results){
       var bod=body.results[i];
-      if(bod.components._type == "city" || bod.components._type== "state" || bod.components._type=="county"){
-        if((bod.components.city.toUpperCase()== city.toUpperCase() || bod.components.state.toUpperCase()==city.toUpperCase() || bod.components.county.toUpperCase() ==city.toUpperCase()) && bod.components.country.toUpperCase()== country.toUpperCase()){
+      if(bod.components._type == "city" || bod.components._type== "state" || bod.components._type=="county" || bod.components._type=="town"){
+        if((bod.components.state.toUpperCase()== city.toUpperCase()  && bod.components.country.toUpperCase()== country.toUpperCase() )){
           callback(bod.geometry.lat+" "+bod.geometry.lng);
           break;
         }
-      }
+        else if (bod.components.city.toUpperCase()==city.toUpperCase() && bod.components.country.toUpperCase()== country.toUpperCase()) {
+          callback(bod.geometry.lat+" "+bod.geometry.lng);
+          break;
+        }
+        else if(bod.components.county.toUpperCase() ==city.toUpperCase()&& bod.components.country.toUpperCase()== country.toUpperCase()){
+          callback(bod.geometry.lat+" "+bod.geometry.lng);
+          break;
+        }
+        else if (bod.components.town.toUpperCase()== city.toUpperCase()&& bod.components.country.toUpperCase()== country.toUpperCase()) {
+          callback(bod.geometry.lat+" "+bod.geometry.lng);
+          break;
+        }
+        }
+
+
     //  console.log(body.results[i].components)
     }
   });
